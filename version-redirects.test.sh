50a19f45fc9aa59139d104e14dffca31a0bd6362
Mark Waite <mark.earl.waite@gmail.com>
2020-06-29 12:22:20 -0600

    Adapt update site acceptance tests to latest UC
    The latest update center release includes major improvements from
    Daniel Beck.  The improvements have changed some of the redirects.
    
    This pull requests describes the current redirect behavior.

--- version-redirects.test.sh
+++ version-redirects.test.sh
@@ -26,11 +26,33 @@ function checkRedirect() {
   fi
 }
 
-checkRedirect "2.204"                   "2.204"
-checkRedirect "2.204.1"          "stable-2.204"
-checkRedirect "2.204.3"          "stable-2.204"
-checkRedirect "2.204.3.1"        "stable-2.204"
-checkRedirect "2.204.3-SNAPSHOT" "stable-2.204"
-checkRedirect "2.204.3-1234567"  "stable-2.204"
+checkRedirect "2.204"            "dynamic-2.200"          # Weekly falls back
+
+checkRedirect "2.204.1"          "dynamic-stable-2.204.6" # LTS takes latest release on stable branch
+checkRedirect "2.204.3"          "dynamic-stable-2.204.6"
+checkRedirect "2.204.6"          "dynamic-stable-2.204.6"
+
+checkRedirect "2.204.3.1"        "current"                # Unrecognized version takes current
+checkRedirect "2.204.3-SNAPSHOT" "current"
+checkRedirect "2.204.3-1234567"  "current"
+
+checkRedirect "2.222"            "dynamic-2.217"          # Weekly falls back
+
+checkRedirect "2.222.1"          "dynamic-stable-2.222.4" # LTS takes latest release on stable branch
+checkRedirect "2.222.3"          "dynamic-stable-2.222.4"
+checkRedirect "2.222.4"          "dynamic-stable-2.222.4"
+
+checkRedirect "2.222.3.1"        "current"                # Unrecognized version takes current
+checkRedirect "2.222.3-SNAPSHOT" "current"
+checkRedirect "2.222.3-1234567"  "current"
+
+checkRedirect "2.235"            "dynamic-2.223"          # Weekly falls back
+
+checkRedirect "2.235.1"          "dynamic-stable-2.222.4" # Should take latest release on stable branch?
+checkRedirect "2.235.1"          "dynamic-stable-2.222.4"
+
+checkRedirect "2.235.1.1"        "current"                # Unrecognized version takes current
+checkRedirect "2.235.1-SNAPSHOT" "current"
+checkRedirect "2.235.1-1234567"  "current"
 
 exit $result
