bac017c40273c58f77113a33c818533650124e34
smerle33 <smerle@cloudbees.com>
2022-09-26 09:08:11 +0200

    add a new java version check from maven output

--- checks.sh
+++ checks.sh
@@ -9,46 +9,46 @@ failed=0
 
 uname -a
 if test -e /etc/os-release; then
-    cat /etc/os-release
+	cat /etc/os-release
 fi
 
 if test -e /proc/cpuinfo; then
-    cat /proc/cpuinfo
+	cat /proc/cpuinfo
 fi
 
 if test -e /proc/meminfo; then
-    cat /proc/meminfo
+	cat /proc/meminfo
 fi
 
 if [[ "$(locale -a)" =~ ${DefaultLocale} ]]; then
-    echo "${DefaultLocale} locale is available"
+	echo "${DefaultLocale} locale is available"
 else
-    echo "ERROR: ${DefaultLocale} locale is not available $(locale -a)"
-    failed=$((failed + 1))
+	echo "ERROR: ${DefaultLocale} locale is not available $(locale -a)"
+	failed=$((failed + 1))
 fi
 
 if getent passwd jenkins >/dev/null; then
-    echo "'jenkins' user exists"
+	echo "'jenkins' user exists"
 else
-    echo "ERROR: 'jenkins' user does not exist"
-    failed=$((failed + 2))
+	echo "ERROR: 'jenkins' user does not exist"
+	failed=$((failed + 2))
 fi
 
 if [[ "$(whoami)" != "jenkins" ]]; then
-    echo "ERROR: Not running as 'jenkins' user"
-    echo "whoami: $(whoami)"
-    failed=$((failed + 4))
+	echo "ERROR: Not running as 'jenkins' user"
+	echo "whoami: $(whoami)"
+	failed=$((failed + 4))
 fi
 
 if sudo -n whoami; then
-    echo "ERROR: running as root should not be possible"
-    failed=$((failed + 8))
+	echo "ERROR: running as root should not be possible"
+	failed=$((failed + 8))
 fi
 
 set +u
 if [[ -z "${JAVA_HOME}" ]]; then
-    echo "ERROR: the 'JAVA_HOME' environment variable is undefined"
-    failed=$((failed + 16))
+	echo "ERROR: the 'JAVA_HOME' environment variable is undefined"
+	failed=$((failed + 16))
 fi
 set -u
 
@@ -57,35 +57,79 @@ set -u
 # Java 11 needs to include 'jdk-11' in the directory structure
 # Java 17 needs to include 'jdk-17' in the directory structure
 if [ $# -ge 1 ] && [ -n "$1" ]; then
-    echo "label of the node: $1"
-    jdk="${DefaultJDKVersion}"
-    if [ "$1" = "maven-8" ]; then
-        jdk="jdk-8"
-    elif [ "$1" = "maven-17" ]; then
-        jdk="jdk-17"
-    elif [ "$1" = "maven" ]; then
-        jdk="jdk-8"
-    elif [ "$1" = "jdk8" ]; then
-        jdk="jdk-8"
-    elif [ "$1" = "kubernetes" ]; then
-        jdk="jdk-8"
-    fi
-    if [[ "$(mvn -v 2>&1)" != *"${jdk}"* ]]; then
-        echo "ERROR: JDK not matching the expected ${jdk} for label '$1'"
-        echo "$(mvn -v 2>&1)"
-        failed=$((failed + 32))
-    else
-        echo "JDK ok ${jdk} for $1"
-    fi
+	echo "label of the node: $1"
+	jdk="${DefaultJDKVersion}"
+	if [ "$1" = "maven-8" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "maven-17" ]; then
+		jdk="jdk-17"
+	elif [ "$1" = "maven" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "jdk8" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "kubernetes" ]; then
+		jdk="jdk-8"
+	fi
+	if [[ "$(mvn -v 2>&1)" != *"${jdk}"* ]]; then
+		echo "ERROR: JDK not matching the expected ${jdk} for label '$1'"
+		echo "$(mvn -v 2>&1)"
+		failed=$((failed + 32))
+	else
+		echo "JDK ok ${jdk} for $1"
+	fi
 fi
 
-if [[ "$(mvn -v  2>&1)" != *"${DefaultMavenVersion}"* ]]; then
-    echo "ERROR Maven version not matching what is expected : "
-    echo "expecting ${DefaultMavenVersion} for label '$1'"
-    echo "found $(mvn -v 2>&1)"
-    failed=$((failed + 64))
+# This check relies on the java version output of the 'mvn -v' command
+# Java 8 needs to include '1.8' in the output
+# Java 11 needs to include '11' in the output
+# Java 17 needs to include '17' in the output
+if [ $# -ge 1 ] && [ -n "$1" ]; then
+	echo "label of the node: $1"
+	jdk="${DefaultJDKVersion}"
+	if [ "$1" = "maven-8" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "maven-17" ]; then
+		jdk="jdk-17"
+	elif [ "$1" = "maven" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "jdk8" ]; then
+		jdk="jdk-8"
+	elif [ "$1" = "kubernetes" ]; then
+		jdk="jdk-8"
+	fi
+	case ${jdk} in
+	jdk-8)
+		jdknumber="1\.8"
+		;;
+	jdk-11)
+		jdknumber="11\."
+		;;
+	jdk-17)
+		jdknumber="17\."
+		;;
+	*)
+		echo "ERROR: JDK not matching the expected ${jdk} for label '$1'"
+		echo "$(mvn -v 2>&1)"
+		failed=$((failed + 128))
+		;;
+	esac
+
+	if [[ "$(mvn -v 2>&1 | grep 'JAVA Version')" != *"${jdknumber}"* ]]; then
+		echo "ERROR: JDK not matching the expected ${jdknumber} for label '$1'"
+		echo "$(mvn -v 2>&1)"
+		failed=$((failed + 128))
+	else
+		echo "JDK Version ok ${jdknumber} for $1"
+	fi
+fi
+
+if [[ "$(mvn -v 2>&1)" != *"${DefaultMavenVersion}"* ]]; then
+	echo "ERROR Maven version not matching what is expected : "
+	echo "expecting ${DefaultMavenVersion} for label '$1'"
+	echo "found $(mvn -v 2>&1)"
+	failed=$((failed + 64))
 else
-    echo "Maven version ${DefaultMavenVersion} OK for label '$1'"
+	echo "Maven version ${DefaultMavenVersion} OK for label '$1'"
 fi
 
 exit ${failed}
