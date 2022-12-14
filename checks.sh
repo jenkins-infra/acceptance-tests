7ca4b72f062fdd7b8baf9f1bff01c7439afcd4e3
Damien Duportal <damien.duportal@gmail.com>
2022-12-14 09:26:21 +0100

    wip: shellcheck + support for JDK17 and 19

--- checks.sh
+++ checks.sh
@@ -24,61 +24,61 @@ if [[ "$(locale -a)" =~ ${DefaultLocale} ]]; then
 	echo "${DefaultLocale} locale is available"
 else
 	echo "ERROR: ${DefaultLocale} locale is not available $(locale -a)"
-	failed=$(($failed + 1))
+	failed=$((failed + 1))
 fi
 
 if getent passwd jenkins >/dev/null; then
 	echo "'jenkins' user exists"
 else
 	echo "ERROR: 'jenkins' user does not exist"
-	failed=$(($failed + 2))
+	failed=$((failed + 2))
 fi
 
 if [[ "$(whoami)" != "jenkins" ]]; then
 	echo "ERROR: Not running as 'jenkins' user"
 	echo "whoami: $(whoami)"
-	failed=$(($failed + 4))
+	failed=$((failed + 4))
 fi
 
 if sudo -n whoami; then
 	echo "ERROR: running as root should not be possible"
-	failed=$(($failed + 8))
+	failed=$((failed + 8))
 fi
 
 set +u
 if [[ -z "${JAVA_HOME}" ]]; then
 	echo "ERROR: the 'JAVA_HOME' environment variable is undefined"
-	failed=$(($failed + 16))
+	failed=$((failed + 16))
 fi
 set -u
 
 # This check relies on the path of the Java runtime
-# Java 8 needs to include 'jdk-8' in the directory structure
-# Java 11 needs to include 'jdk-11' in the directory structure
-# Java 17 needs to include 'jdk-17' in the directory structure
+# Java 8 needs to include 'jdk-8' in the directory structure, Java 11 'jdk-11', etc.
 if [ $# -ge 1 ] && [ -n "$1" ]; then
 	echo "label of the node: $1"
 	jdk="${DefaultJDKVersion}"
-	if [ "$1" = "maven-8" ]; then
-		jdk="jdk-8"
-	elif [ "$1" = "maven-17" ]; then
-		jdk="jdk-17"
-	elif [ "$1" = "jdk17" ]; then
-		jdk="jdk-17"
-	elif [ "$1" = "maven" ]; then
-		jdk="jdk-8"
-	elif [ "$1" = "jdk8" ]; then
-		jdk="jdk-8"
-	elif [ "$1" = "kubernetes" ]; then
-		jdk="jdk-8"
-	fi
+
+  case "$1" in
+  maven,maven-8,jdk-8,kubernetes)
+    jdk="jdk-8";;
+  maven-11,jdk-11)
+    jdk="jdk-11";;
+  maven-17,jdk-17)
+    jdk="jdk-17";;
+  maven-19,jdk-19)
+    jdk="jdk-19";;
+  *)
+    echo "Label '$1' specified. Using default jdk."
+  esac
+
 	if [[ "$(mvn -v 2>&1)" != *"${jdk}"* ]]; then
 		echo "ERROR: JDK not matching the expected ${jdk} for label '$1'"
-		echo "$(mvn -v 2>&1)"
-		failed=$(($failed + 32))
+		mvn -v 2>&1
+		failed=$((failed + 32))
 	else
 		echo "JDK ok ${jdk} for $1"
 	fi
+
 fi
 
 # This check relies on the java version output of the 'mvn -v' command
@@ -114,14 +114,14 @@ if [ $# -ge 1 ] && [ -n "$1" ]; then
 	*)
 		echo "ERROR: JDK not matching the expected ${jdk} for label '$1'"
 		mvn -v 2>&1
-		failed=$(($failed + 64))
+		failed=$((failed + 64))
 		;;
 	esac
 
     JDKfromMaven=$(mvn -v 2>&1 | grep "Java version" | cut -d " " -f 3)
 	if [[ "${JDKfromMaven}" != *"${jdknumber}"* ]]; then
 		echo "ERROR: JDK from maven ${JDKfromMaven} not matching the expected ${jdknumber} for label '$1'"
-		failed=$(($failed + 64))
+		failed=$((failed + 64))
 	else
 		echo "JDK Version ok ${JDKfromMaven} for $1"
 	fi
@@ -129,7 +129,7 @@ fi
 
 if [[ "$(mvn -v 2>&1)" != *"${DefaultMavenVersion}"* ]]; then
 	echo "ERROR Maven version not matching what is expected : expecting ${DefaultMavenVersion} for label '$1' found $(mvn -v 2>&1)"
-	failed=$(($failed + 128))
+	failed=$((failed + 128))
 else
 	echo "Maven version ${DefaultMavenVersion} OK for label '$1'"
 fi
