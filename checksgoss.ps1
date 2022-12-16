7b76c41df9bc00d2ae00213fdc13e92a5af25aae
smerle33 <smerle@cloudbees.com>
2022-12-16 15:22:57 +0100

    WIP

--- checksgoss.ps1
+++ checksgoss.ps1
@@ -9,10 +9,10 @@ $ErrorActionPreference = 'Stop'
 # Disable Progress bar for faster downloads
 $ProgressPreference = 'SilentlyContinue'
 
-$env:GOSS_USE_ALPHA=1
-$env:GOSS_VERSION=0.3.20
+$GOSS_USE_ALPHA=1
+$GOSS_VERSION=0.3.20
 
-Write-Host "https://github.com/goss-org/goss/releases/download/v${env:GOSS_VERSION}/goss-alpha-windows-amd64.exe"
-Invoke-WebRequest "https://github.com/goss-org/goss/releases/download/v${env:GOSS_VERSION}/goss-alpha-windows-amd64.exe" -OutFile "C:\\tools\\goss.exe"
+Write-Host "https://github.com/goss-org/goss/releases/download/v$GOSS_VERSION/goss-alpha-windows-amd64.exe"
+Invoke-WebRequest "https://github.com/goss-org/goss/releases/download/v$GOSS_VERSION/goss-alpha-windows-amd64.exe" -OutFile "C:\\tools\\goss.exe"
 
 Invoke-Command {& goss -g ./goss-windows.yaml validate --format documentation;}
